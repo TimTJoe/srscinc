bb3c85fc76a87b010b6472df30d9678b20198279
Timothy T. Joe <106974662+TimTJoe@users.noreply.github.com>
2024-06-24 11:09:10 +0000

    added courses section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
 document.getElementById("menuIcon").addEventListener("click", function () {
   document.getElementById("navList").classList.toggle("show");
 });
+
+let currentIndex = 0;
+const cardsWrapper = document.querySelector(".cards-wrapper");
+const cardWidth = document.querySelector(".card").offsetWidth;
+const cards = document.querySelectorAll(".card");
+const numCards = cards.length;
+const visibleCards = 2; // Number of cards visible at a time
+
+function slideNext() {
+  if (currentIndex < numCards - visibleCards) {
+    currentIndex++;
+    cardsWrapper.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
+  }
+}
+
+function slidePrev() {
+  if (currentIndex > 0) {
+    currentIndex--;
+    cardsWrapper.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
+  }
+}
+
